4189e47088a4f93c114113b438590bb99c203796
정동민 <dmjeong@whlie.co.kr>
2025-01-13 16:40:09 +0900

    gulp 세팅 변경

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
-# PortFolio_MAIN
-포트폴리오 메인
-
-자동 배포 주소 (운영버전 - main 브랜치)
-https://
-
-자동 배포 주소 (개발버전 - develop 브랜치)
-https://
-
-## 폴더 구조
-```
 folder/
 |– assets/				
-|   |– common/					# pc/app 공통사용
-|   |   |– fonts/				# 공통사용 폰트
-|   |– mo/	            		# APP/MO
+|   |– common/			# pc/app 공통사용
+|   |   |– fonts/		# 공통사용 폰트
+|   |– mo/	# APP/MO
 |   |   |– css/
 |   |   |    └─style.min.css 	# 컴파일 css
 |   |   |– images/
@@ -22,7 +11,7 @@ folder/
 |   |   |– js/
 |   |   |    |– plugin/		 	
 |   |   |    └─ui.common.js 	
-|   |– pc/						# PC
+|   |– pc/	# PC
 |   |   |– css/
 |   |   |    └─style.min.css 	# 컴파일 css
 |   |   |– images/
@@ -31,16 +20,15 @@ folder/
 |   |   └─ js/
 |   |       |– plugin/		 	
 |   |       └─ui.common.js 	
-|   └─ images/					# pc/app 공통사용 이미지 경로
-|– html/						# APP/MO 컴파일 html 파일
-|– pc_html/						# pc 컴파일 html 파일
-|– pc_src/						# pc 작업 파일 경로
+|   └─ images/			# pc/app 공통사용 이미지 경로
+|– html/			# APP/MO 컴파일 html 파일
+|– pc_html/			# pc 컴파일 html 파일
+|– pc_src/			# mo 작업 파일 경로
 |   |– html/			
 |   |– scss/			
 |   └─js/				
-|– src/							# mo 작업 파일 경로
+|– src/				# mo 작업 파일 경로
 |   |– html/				
 |   |– scss/				
 |   └─js/				
 |
-```
--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,3 +1,4 @@
+// // gulp 및 라이브러리 연결
 import gulp from 'gulp';
 import fileinclude from 'gulp-file-include';
 // html 코드 정리
@@ -7,79 +8,47 @@ import browserSync from 'browser-sync';
 import sass from 'gulp-dart-sass';
 import sourcemaps from 'gulp-sourcemaps';
 // 파일 이름을 변경
-// import rename from 'gulp-rename';
+import rename from 'gulp-rename';
 // css 파일 압축
 // import cssmin from 'gulp-cssmin';
 // 변경사항을 감지 - 변경될 때마다 특정 작업 실행
 import watch from 'gulp-watch';
 // 벤더 프리픽서 - css 크로스브라우징 자동으로 되도록 속성 추가해줌 ex)'-webkit-'
 import autoprefixer from 'gulp-autoprefixer';
-// import babel from 'gulp-babel';
 
-// APP, MO
+// BrowserSync 인스턴스 생성
+// const bs = browserSync.create();
+// 경로 변수 지정
 const dir = {
 	src: './src/',
 	dist: './'
-}
+};
 const src = {
 	html: dir.src + 'html/',
 	css: dir.src + 'scss/',
-	js: dir.src + 'js/'
+	js: dir.src + 'js/',
+	// img: dir.src + 'html/**/img/'
 }
 const dist = {
 	html: dir.dist + 'html/',
-	css: dir.dist + 'assets/mo/css/',
-	js: dir.dist + 'assets/mo/js/'
-}
-// PC
-const pcDir = {
-	src: './pc_src/',
-	dist: './'
-}
-const pcSrc = {
-	html: pcDir.src + 'html/',
-	css: pcDir.src + 'scss/',
-	js: pcDir.src + 'js/'
-}
-const pcDist = {
-	html: pcDir.dist + 'pc_html/',
-	css: pcDir.dist + 'assets/pc/css/',
-	js: pcDir.dist + 'assets/pc/js/'
+	css: dir.dist + 'assets/css/',
+	js: dir.dist + 'assets/js/',
+	// img: dir.dist + 'assets/img/'
 }
 
-// scssOptions
-const scssOptions = {
-	sass: {
-		outputStyle: 'compressed',
-		indentType: 'tab',
-		indentWidth: 1
-	}
-};
-// pxtoviewport
-// const processors = [
-// 	pxtoviewport({
-// 		viewportWidth: 375,
-// 		unitToConvert: 'px',
-// 		propList: ['*', '!border', '!border-top', '!border-left', '!border-bottom', '!border-right'],
-// 		unitPrecision: 5,
-// 		minPixelValue: 1
-// 	})
-// ];
 
-// mo:html 처리
-gulp.task('mo:html', function(){
-	return gulp
-	.src([
+// html 처리
+gulp.task('html', function(){
+	return gulp.src([
 		src.html + '*.html',
 		src.html + '**/*.html',
 		src.html + '**/*.css',
-		'!' +  src.html + '/_partials/*.html'
-		// '!' +  src.html + '/guide/_partials/*.html'
+		// src.img + "**/*",
+		'!' + src.html + '_partials/*.html'
 	])
 	.pipe(fileinclude({
 		prefix: '@@',
-		basepath: '@file',
-		context: {}
+		basepath: '@file'
 	}))
 	.pipe(beautifycode({
 		indent_size: 4,
@@ -87,125 +56,70 @@ gulp.task('mo:html', function(){
 	}))
 	// 처리된 html파일을 /html에 저장
 	.pipe(gulp.dest(dist.html))
+	// .pipe(gulp.dest(dist.img))
 	.pipe(browserSync.reload({ stream: true }))
-})
+});
 
-// mo:css 처리
-gulp.task('mo:sass', () => {
-	return gulp
-	.src([
-		src.css + '/**/*.scss', 
-		src.css + 'style.scss', 
-		'!' + src.css + '/vendors/*.scss'
-		// '!' + src.css + '/tenant-style.scss',
-		// '!' + src.css + '/traveler-style.scss'
-	])
+
+// css 처리
+gulp.task('sass', function(){
+	const scssOption = {
+		sass: {
+			outputStyle: 'compressed',
+			indentType: 'tab',
+			indentWidth: 1
+		}
+	};
+	return gulp.src([src.css + '*.scss', src.css + '/**/*.scss'])
 	.pipe(sourcemaps.init())
-	.pipe(sass(scssOptions).on('error', sass.logError))
+	.pipe(sass(scssOption).on('error', sass.logError))
 	.pipe(autoprefixer())
-	// .pipe(postcss(processors))
 	.pipe(sourcemaps.write('./'))
+	// .pipe(cssmin())
+	// 처리된 sass파일을 assets/css/에 1차 저장
 	.pipe(gulp.dest(dist.css))
 	// .pipe(rename('style.min.css'))
-	// .pipe(cssmin())
+	// 1차 저장된 css들을 이름을 바꾸고 압축해서 2차저장
 	// .pipe(gulp.dest(dist.css))
 	.pipe(browserSync.reload({ stream: true }))
-})
-// // tenant css
-// gulp.task('tenant:sass', () => {
-// 	return gulp
-// 		.src([
-// 			src.css + '/**/*.scss', 
-// 			src.css + 'tenant-style.scss', 
-// 			'!' + src.css + '/vendors/*.scss', 
-// 			'!' + src.css + '/style.scss',
-// 			'!' + src.css + '/traveler-style.scss'
-// 		])
-// 		.pipe(sourcemaps.init())
-// 		.pipe(sass(scssOptions).on('error', sass.logError))
-// 		.pipe(autoprefixer())
-// 		.pipe(sourcemaps.write('./'))
-// 		.pipe(gulp.dest(dist.css))
-// 		.pipe(rename('tenant-style.min.css'))
-// 		.pipe(cssmin())
-// 		.pipe(gulp.dest(dist.css))
-// 		.pipe(browserSync.reload({ stream: true }));
-// });
-// js
-gulp.task('mo:js', () => {
-	return gulp
-		.src([
-			src.js + '/**/*.js', 
-			src.js + '*.js'
-		])
-		.pipe(gulp.dest(dist.js))
-		.pipe(browserSync.reload({ stream: true }))
 });
 
-// PC html
-gulp.task('pc:html', () => {
-	return gulp
-		.src([
-			pcSrc.html + '*.html', 
-			pcSrc.html + '/**/*.html', 
-			pcSrc.html + '/**/*.css', '!' +  
-			pcSrc.html + '/_partials/*.html', 
-			'!' +  pcSrc.html + '/guide/_partials/*.html'
-		])
-		.pipe(fileinclude({
-			prefix: '@@',
-			basepath: '@file',
-			context: {}
-		}))
-		.pipe(beautifycode({
-			indent_size: 4,
-			indent_with_tabs: true
-		}))
-		.pipe(gulp.dest(pcDist.html))
-		.pipe(browserSync.reload({ stream: true }));
+// js 처리
+gulp.task('js', function(){
+	return gulp.src([
+		src.js + '*.js', 
+		src.js + '/**/*.js',
+		'!' + src.js + 'plugin/**'
+	])
+	.pipe(rename({ extname: '.min.js'}))
+	.pipe(gulp.dest(dist.js))
+	.pipe(browserSync.reload({ stream: true }));
 });
-// PC css
-gulp.task('pc:sass', () => {
-	return gulp
-		.src([pcSrc.css + '/**/*.scss', pcSrc.css + '*.scss', '!' + pcSrc.css + '/vendors/*.scss'])
-		.pipe(sourcemaps.init())
-		.pipe(sass(scssOptions).on('error', sass.logError))
-		.pipe(autoprefixer())
-		// .pipe(postcss(processors))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(pcDist.css))
-		// .pipe(rename('style.min.css'))
-		// .pipe(cssmin())
-		// .pipe(gulp.dest(pcDist.css))
-		.pipe(browserSync.reload({ stream: true }))
-});
-// PC js
-gulp.task('pc:js', () => {
-	return gulp
-		.src([
-			pcSrc.js + '/**/*.js', 
-			pcSrc.js + '*.js'
-		])
-		.pipe(gulp.dest(pcDist.js))
-		.pipe(browserSync.reload({ stream: true }))
-});
+
+// // img 처리
+// gulp.task('img', function(){
+// 	return gulp.src([
+// 		src.img + '*.*',
+// 		src.img + '/**/*.*',
+// 		src.img + '/**/**/*.*'
+// 	])
+// 	.pipe(gulp.dest(dist.img))
+// 	.pipe(browserSync.reload({ stream: true }));
+// })
 
 gulp.task('watch', function(){
 	browserSync.init({
-		port:3003,
+		port: 3009,
 		server: {
-			index: "/pc_html/index.html"
+			index: "./html/main/main.html"
 		}
 	});
-
-	watch(src.html, gulp.series('mo:html'));
-	watch(src.css, gulp.series(['mo:sass'/* , 'tenant:sass' */]));
-	watch(src.js, gulp.series('mo:js'));
-
-	watch(pcSrc.html, gulp.series('pc:html'));
-	watch(pcSrc.css, gulp.series(['pc:sass']));
-	watch(pcSrc.js, gulp.series('pc:js'));
+	watch(src.html, gulp.series('html'));
+	watch(src.css, gulp.series('sass'));
+	watch(src.js, gulp.series('js'));
+	// watch(src.img, gulp.series('img'));
 });
 
-// default start
+// default - 터미널에 gulp 명령어 입력하면 실행되는 작업. 여기서는 watch가 실행
 gulp.task('default', gulp.series(['watch']));
+
